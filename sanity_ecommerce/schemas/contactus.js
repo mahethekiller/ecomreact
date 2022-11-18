9d68ee704d5bc26c52dbbee888449d7496403b14
mahethekiller <mahethekiller@gmail.com>
2022-11-18 16:39:09 +0530

    Initial Push

---
+++ sanity_ecommerce/schemas/contactus.js
@@ -0,0 +1,22 @@
+export default {
+  name: "contact",
+  title: "Contact",
+  type: "document",
+  fields: [
+    {
+      name: "address",
+      title: "Address",
+      type: "string",
+    },
+    {
+      name: "contact",
+      title: "Contact",
+      type: "string",
+    },
+    {
+      name: "details",
+      title: "Details",
+      type: "string",
+    },
+  ],
+};
